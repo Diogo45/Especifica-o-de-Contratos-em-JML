677c39c401567fb0caabeb72ac3bc5d08109c05e
Jyurei <jujumelgare@gmail.com>
2017-03-27 12:35:20 -0300

    Update Repositorio.java
    coloquei os metodo

--- Repositorio.java
+++ Repositorio.java
@@ -4,4 +4,24 @@ public class Repositorio{
     public Repositorio(){
         moedas = new ArrayList<>();
     }
+    
+    public boolean add(int m){
+    
+    }
+    
+    public ArrayList<Integer> getMoedas(){
+    
+    }
+    
+    public int getTotal(){
+    
+    }
+    
+    public int remove(int m){
+    
+    }
+    
+    public void reset(){
+    
+    }
 }
