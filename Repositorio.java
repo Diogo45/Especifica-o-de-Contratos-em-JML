43df2049a26811629aae5fe1c6cb27f28a16280e
Jyurei <jujumelgare@gmail.com>
2017-03-27 13:28:33 -0300

    Update Repositorio.java
    falta só o jml do delet

--- Repositorio.java
+++ Repositorio.java
@@ -1,26 +1,46 @@
+import java.util*;
 public class Repositorio{
-    ArrayList<Integer> moedas;
-
+    private /*@ spec_public nullable @*/ArrayList<Integer> moedas;
+    //@invariant moedas != null 
+    /*@
+    ensures moedas.size() == 0;
+    ensures moedas.instanceOf(ArrayList);
+    @*/
     public Repositorio(){
-        moedas = new ArrayList<>();
+        moedas = new ArrayList<Integer>();
     }
-    
+    /*@
+    requires m == 100 || m == 50 || m == 25 || m == 10 || m == 5 || m == 1;
+    ensures (\forall int i;
+                0<=i && i<moedas.size()-1;
+                moedas.get(i) == \old moedas.get(i));
+    ensures moedas.get(moedas.size()-1) == m;
+    ensures moedas.size() == \old moedas.size()+1;
+    @*/
     public boolean add(int m){
     
     }
-    
-    public ArrayList<Integer> getMoedas(){
-    
-    }
-    
-    public int getTotal(){
+    /*@
+    ensures \result == moedas.size();
+    @*/
+    public /*@ pure @*/ int getMoedas(){
     
     }
+    /*@
+    ensures \result == (\sum int i; 0 <= i && i < moedas.size(); moedas.get(i));
+    @*/
+    public /*@ pure @*/ int getTotal(){
     
+    }
+    /*@
+    
+    @*/
     public int remove(int m){
     
     }
-    
+    /*@
+    ensures moedas.size() == 0;
+    @*/
     public void reset(){
     
     }
